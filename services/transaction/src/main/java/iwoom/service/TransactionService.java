26eff3376a03d7527f64876f9bd3501f2b4d9aaf
nicestzz <38760191+nicestzz@users.noreply.github.com>
2019-09-22 10:46:40 -0400

    new changes

--- services/transaction/src/main/java/iwoom/service/TransactionService.java
+++ services/transaction/src/main/java/iwoom/service/TransactionService.java
@@ -1,8 +1,6 @@
 package iwoom.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.jayway.jsonpath.DocumentContext;
-import com.jayway.jsonpath.JsonPath;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
@@ -11,9 +9,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.ArrayList;
-import java.util.HashMap;
 
 @Service
 public class TransactionService {
@@ -33,8 +28,7 @@ public class TransactionService {
         this.objectMapper = objectMapper;
     }
 
-    //public ResponseEntity<String> getAccountByCustomer(String customerId){
-    public ResponseEntity<String> getTransaction(String transId){
+    public ResponseEntity<String> getTransactionById(String transId){
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", apiKey);
 
